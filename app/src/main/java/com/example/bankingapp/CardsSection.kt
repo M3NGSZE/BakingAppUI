fd27590bfe0215c90725e2e3d08bf2fa9fa18ece
M3NGSZE <mengse.thun13@gmail.com>
2025-08-26 23:54:52 +0700

    finish cards sectoin but still a bit odd with heigh

--- app/src/main/java/com/example/bankingapp/CardsSection.kt
+++ app/src/main/java/com/example/bankingapp/CardsSection.kt
@@ -1,16 +1,33 @@
 package com.example.bankingapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bankingapp.data.Card
 import com.example.bankingapp.ui.theme.BlueEnd
 import com.example.bankingapp.ui.theme.BlueStart
 import com.example.bankingapp.ui.theme.GreenEnd
 import com.example.bankingapp.ui.theme.GreenStart
+import com.example.bankingapp.ui.theme.OrangeEnd
 import com.example.bankingapp.ui.theme.OrangeStart
 import com.example.bankingapp.ui.theme.PurpleEnd
 import com.example.bankingapp.ui.theme.PurpleStart
@@ -34,8 +51,8 @@ val cards = listOf(
         cardType = "VISA",
         cardNumber = "078 3467 3446 7899",
         cardName = "School",
-        balance = 4.467,
-        color = getGradient(OrangeStart, OrangeStart),
+        balance = 3.467,
+        color = getGradient(OrangeStart, OrangeEnd),
     ),
     Card(
         cardType = "MASTER CARD",
@@ -55,8 +72,9 @@ fun getGradient(
     )
 }
 
+@Preview
 @Composable
-fun CardSection(){
+fun CardsSection(){
     LazyRow {
         items(cards.size){ index ->
             CardItem(index)
@@ -76,6 +94,49 @@ fun CardItem(
 
     var image = painterResource(id = R.drawable.ic_visa)
     if (card.cardType == "MASTER CARD") {
-        image =
+        image = painterResource(id = R.drawable.ic_mastercard)
+    }
+
+    Box(
+        modifier = Modifier
+            .padding(16.dp, end = lastItemPaddingEnd)
+    ){
+        Column (
+            modifier = Modifier
+                .clip(RoundedCornerShape(25.dp))
+                .background(card.color)
+                .width(250.dp)
+                .height(160.dp)
+                .clickable {}
+                .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = image,
+                contentDescription = card.cardName,
+                modifier = Modifier.width(60.dp)
+            )
+
+            Text(
+                text = card.cardName,
+                color = Color.White,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "$ ${card.balance}",
+                color = Color.White,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = card.cardNumber,
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
